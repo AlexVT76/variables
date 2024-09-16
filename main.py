fddd9ab86f9ecd3d5486509c0c576fda408220cb
AlexVT76 <tokarevalex76@gmail.com>
2024-09-16 10:02:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+homework=12
+hours_count=1.5
+course_name= 'Python'
+print('Курс:',course_name,',всего задач:',homework,',затрачено часов:',hours_count,',cреднеевремя выполнения',hours_count/homework,'часа.')
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
